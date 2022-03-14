2f0041366cd2be93c6cdf81ec6bb2ee8c75c0b85
Andrew Prokop <ajprokop@avaya.com>
2022-03-14 17:49:32 -0500

    Removed all hardcoded XML

--- inboundxml.js
+++ inboundxml.js
@@ -41,25 +41,22 @@ app.post('/cpaas-request/', function (req, res) {
     //CPaaS Inbound XML Parameters	
     var from = req.body.From;
     var to = req.body.To;
-	console.log("Inbound request: " + from + " " + to);
 	
 	var hangup = false;
 	var prompt = "Say something";
 	if ("SpeechResult" in req.body) { 
-		console.log(req.body.SpeechResult);
-		console.log("Digits: " + req.body.Digits);
 		prompt = "I heard you say " + req.body.SpeechResult;
 		if (req.body.SpeechResult == "goodbye") {
 			hangup = true;
 			prompt = "Goodbye";
 		} else if (req.body.SpeechResult == "transfer") {
-			processTransfer("You are about to be transfered.", req, res);
+			processTransfer("You are about to be transfered.", res);
 			return;
 		} else if (req.body.SpeechResult == "redirect") {
-			processRedirect(req, res);
+			processRedirect(res);
 			return;
 		} else if (req.body.SpeechResult == "play") {
-			processPlay(req, res);
+			processPlay(res);
 			return;
 		}
 	}
@@ -71,49 +68,81 @@ app.post('/cpaas-redirect/', function (req, res) {
 	sendResponseToCPaaS("This is the redirect XML web hook.  Goodbye!", true, res);
 });
 
-async function processRedirect(req, res) {
-	var xml = "<Response>\r\n";
-	xml += "<Redirect>" + REDIRECT_URL + "</Redirect>\r\n";
-	xml += "</Response>";
-	res.type('application/xml');
-	res.send(xml);
+async function processRedirect(res) {
+	var xmlDefinition = generateXMLRedirect();
+    var serverResponse = await buildCPaaSResponse(xmlDefinition);
+    res.type('application/xml');
+    res.send(serverResponse);
 }
 
-async function processPlay(req, res) {	
-	var xml = "<Response>\r\n";
-	xml += "<Play loop='0'>" + "http://www.hochmuth.com/mp3/Haydn_Adagio.mp3" + "</Play>\r\n";
-	xml += "</Response>";
-	res.type('application/xml');
-	res.send(xml);	
+function generateXMLRedirect() {
+	var xml_content = [];
+	var redirect = ix.redirect({
+		url: REDIRECT_URL
+	});
+	xml_content.push(redirect);	
+    var xmlDefinition = ix.response({content: xml_content});
+    return xmlDefinition;	
 }
 
-async function processTransfer(prompt, req, res)
-{	
-	var xml = "<Response>\r\n";
-	xml += "<Say>" + prompt + "</Say>\r\n";
-	xml += "<Dial>" + "{" + TRANSFER_NUMBER + "}" + "</Dial>\r\n";
-	xml += "</Response>";
-	res.type('application/xml');
-	res.send(xml);
+async function processPlay(res) {	
+	var xmlDefinition = generateXMLPlay();
+    var serverResponse = await buildCPaaSResponse(xmlDefinition);
+    res.type('application/xml');
+    res.send(serverResponse);
 }
 
-async function sendResponseToCPaaS(prompt, release, res)
-{
+function generateXMLPlay() {
+	var xml_content = [];
+	var play = ix.play({
+		loop : 0,
+		url: "http://www.hochmuth.com/mp3/Haydn_Adagio.mp3"
+	});
+	xml_content.push(play);	
+    var xmlDefinition = ix.response({content: xml_content});
+    return xmlDefinition;	
+}
+
+async function processTransfer(prompt, res) {	
+	var xmlDefinition = generateXMLTransfer(prompt);
+    var serverResponse = await buildCPaaSResponse(xmlDefinition);
+    res.type('application/xml');
+    res.send(serverResponse);
+}
+
+function generateXMLTransfer(prompt) {
+	var responseXML = ix.response({content: [
+		ix.say({
+			language: enums.Language.EN,
+			text: prompt,
+			voice : enums.Voice.FEMALE
+		}),
+		ix.dial({
+			content : [
+				ix.number({
+					number: TRANSFER_NUMBER
+				})
+			]
+		})		
+	]});	
+    return responseXML;	
+}
+
+async function sendResponseToCPaaS(prompt, release, res) {
 	  var xmlDefinition = generateXMLSpeech(REQUEST_URL, prompt, release);
       var serverResponse = await buildCPaaSResponse(xmlDefinition);
       res.type('application/xml');
       res.send(serverResponse);
 }
 
-function generateXMLSpeech(request_url,  prompt, hangup )
-{
+function generateXMLSpeech(request_url,  prompt, hangup ) {
 	var xml_content = [];
 	if (hangup == false) {
 		var gather = ix.gather({
 			action : request_url,
 			method : "POST",
-			input : "speech dtmf",
-			timeout : "100",
+			input : "speech",
+			timeout : "10",
 			content : [
 				ix.say({
 					language: enums.Language.EN,
@@ -132,21 +161,16 @@ function generateXMLSpeech(request_url,  prompt, hangup )
         hangup = ix.hangup();
         xml_content.push(say);
         xml_content.push(hangup);
-	}
-	
+	}	
     var xmlDefinition = ix.response({content: xml_content});
     return xmlDefinition;
 }
 
-async function buildCPaaSResponse(xmlDefinition)
-{
-      console.log("XML Def: " , xmlDefinition);
+async function buildCPaaSResponse(xmlDefinition) {
       var result = await ix.build(xmlDefinition).then(function(xml){
-          console.log("The XML:" , xml);
           return xml;
       }).catch(function(err){
           console.log('The generated XML is not valid!', err);
       });
-
       return result;
 }
